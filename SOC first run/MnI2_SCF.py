a9a95777c938b965fb5f7e37558923b3af28b6d2
Rune Ekman <runerce@gmail.com>
2026-04-26 11:30:40 +0200

    First SOC run done! SCF took 6h 3m, (saved a 13GB file with mode='all') on 16 cores. Band calculation and spin coloring took 3h 41m on 16 cores (saved a 66mb gpw withOUT mode='all') on 16 cores.
    SCF gpw not included in the repo due to size - if we have things to run using it, contact me.

---
+++ SOC first run/MnI2_SCF.py
@@ -0,0 +1,119 @@
+from gpaw.new.ase_interface import GPAW
+from ase import Atoms
+import numpy as np
+from gpaw import FermiDirac
+from ase.visualize import view
+import ase
+from ase.build import make_supercell
+
+
+#NOTE this is before SOC!
+
+#Parameters that need updating for every material:
+#1) Primitive cell, use different cif input
+#2) Perhaps transformation matrix, depends on if magnetic supercell is the same (ensure same lattice and same Q
+#3) Magnetic moment on each of the magnetic atoms (m)
+
+#Parameters that should be checked for every run:
+#1) The PW cutoff
+#2) The k-point grid-size
+#3) The chosen parallelization in the calc object.
+
+#To do later:
+#1) Generalize the magnetic supercell calculation to a function that takes a lattice type (?), a Q vector, and a handedness (lefthanded vs righthanded spiral rotation) as input, and the output should then be the magnetic supercell.
+
+
+primitive = ase.io.read("1MnI2-1.cif")
+
+name = primitive.get_chemical_formula(mode='metal')
+
+#Running for SOC:
+name += '_SOC_'
+
+#Define transformation matrix from primitive to magnetic cell
+P = np.array([
+    [2, 1, 0],
+    [-1, 1, 0],
+    [0, 0, 1]
+])
+
+supercell = make_supercell(primitive, P)
+
+
+m = 4.5
+magmoms = np.zeros((len(supercell), 3))
+
+
+A = primitive.cell[:2, :2] #extracts the 2D lattice vectors from the PRIMITIVE CELL. It is a 2x2 matrix.
+A_inv = np.linalg.inv(A.T) #Invert the above, constructs a mapping from cartesian to lattice coordinates. Transposed since AS>
+                            #This gives \vec{n}=A^{-1}\vec{r}, where r is cartesian position, n is coordinates in lattice bas>
+Q = np.array([1/3, 1/3])    #Defines the magnetic ordering vector in reciprocal lattice coordinates.
+
+for i, atom in enumerate(supercell):
+    if atom.symbol != 'Mn':
+        continue
+
+    r_cart = atom.position[:2] #extracts the cartesian position of the Mn atom
+    n = A_inv @ r_cart         # Converts to lattice coordinates \vec{r}=n_1a_1+n_2a_2
+
+    phase = 2 * np.pi * np.dot(-Q, n) #each lattice site gets a phase angle depending on its position (given via the lattice >
+    #NOTE minus Q above for clockwise
+
+    magmoms[i] = [                   #Assigns a planar spin spiral, note that m_0=(0,m,0) to get this.
+        -m * np.sin(phase),
+        m * np.cos(phase),
+        0.0
+    ]
+
+
+supercell.set_initial_magnetic_moments(magmoms)
+
+
+
+# ---  Setup GPAW Calculator ---
+# Article specifies: LDA functional, 600 eV cutoff.
+# Symmetry must be off for non-collinear spirals.
+# k-points: The magnetic BZ is smaller.
+calc = GPAW(
+    mode={'name':'pw',
+          'ecut':600},          # 600 eV cutoff in per paper. 
+    xc='LDA',              # Paper explicitly uses LDA 
+    mixer={'backend': 'pulay',              #This was used to mimic https://gpaw.readthedocs.io/tutorialsexercises/magnetic/s>
+                       'beta': 0.05,
+                       'method': 'sum',
+                       'nmaxold': 5,
+                       'weight': 100},
+    kpts={'size':(12,12,1), 'gamma':True},	 # Adjusted for the rhombus supercell
+    symmetry='off',	   # Crucial for spiral
+    magmoms=magmoms,	   # Enforce non-collinear start
+    spinpol=True,          # Needed for non-collinear
+    occupations=FermiDirac(0.01),
+    txt=name+'_SCF_GS.txt',
+    maxiter=100,
+    parallel={'domain':4,'kpt':4,'band':1}, # Attempt at running in parallel for the compute node.
+    soc= True,
+)
+
+calc.verbosity=1
+
+supercell.calc = calc
+
+
+# --- 5. Run SCF Calculation ---
+print("Running SCF for magnetic supercell...")
+energy = supercell.get_potential_energy()
+calc.write(name+'_SCF_GS.gpw',mode='all')
+print('Finished SCF calculation, result saved in gpw file.')
+
+#Checking convergence and magnetic moments
+energy = supercell.get_potential_energy()
+print('energy=',energy) #check for divergence. If it diverges, something wrong in the setup
+print('')
+#Magmoms
+print('Local magnetic moments = ', calc.get_non_collinear_magnetic_moments())
+#We want three Mn moments, roughly 120 degrees apart, magnitude around 3-5 µB
+print('')
+#Total M
+print('Total magnetic moment=', supercell.get_magnetic_moment())
+#Should be 0. If they align ferromagnetically, phase may be incorrectly assigned
+
